783c271adf1ec20027199424e0ebba31e966b453
Matt Steed <matt.steed@gmail.com>
2020-07-02 22:26:50 +0100

    Finalise README

--- course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
+++ course-04/project/c4-final-project-starter-code/backend/src/lambda/auth/auth0Authorizer.ts
@@ -2,24 +2,24 @@ import { CustomAuthorizerEvent, CustomAuthorizerResult } from 'aws-lambda'
 import 'source-map-support/register'
 
 import { verify, decode } from 'jsonwebtoken'
-import { createLogger } from '../../utils/logger'
 import Axios from 'axios'
 import { Jwt } from '../../auth/Jwt'
 import { JwtPayload } from '../../auth/JwtPayload'
 import { getToken } from '../../auth/utils'
 
+import { createLogger } from '../../utils/logger'
 const logger = createLogger('auth')
 
+// TODO: Provide a URL that can be used to download a certificate that can be used
+// to verify JWT token signature.
+// To get this URL you need to go to an Auth0 page -> Show Advanced Settings -> Endpoints -> JSON Web Key Set
 const jwksUrl = process.env.JWKS_URL
 
 export const handler = async (
   event: CustomAuthorizerEvent
 ): Promise<CustomAuthorizerResult> => {
-  logger.info('Authorizing a user', event.authorizationToken)
-
   try {
     const jwtToken = await verifyToken(event.authorizationToken)
-
     logger.info('User was authorized', jwtToken)
 
     return {
@@ -54,15 +54,21 @@ export const handler = async (
   }
 }
 
-const verifyToken = async (authHeader: string): Promise<JwtPayload> => {
+async function verifyToken(authHeader: string): Promise<JwtPayload> {
   const token = getToken(authHeader)
   const jwt: Jwt = decode(token, { complete: true }) as Jwt
 
-  const response = await Axios.get(jwksUrl)
-  const cert_key_id = response.data.keys[0].x5c[0]
-
-  const cert =
-    '-----BEGIN CERTIFICATE-----\n' + cert_key_id + '\n-----END CERTIFICATE-----'
-
-  return verify(token, cert, { algorithms: [jwt.header.alg] }) as JwtPayload
+  await Axios.get(jwksUrl)
+    .then(function(response){
+      logger.info('Number of keys', {keys: response.data.keys.length})
+      const publicKey = certToPEM(response.data.keys[0].x5c[0])
+      verify(token, publicKey)
+    })
+  return jwt.payload
 }
+
+function certToPEM(cert) {
+  cert = cert.match(/.{1,64}/g).join('\n');
+  cert = `-----BEGIN CERTIFICATE-----\n${cert}\n-----END CERTIFICATE-----\n`;
+  return cert;
+};
--- course-04/project/c4-final-project-starter-code/client/src/config.ts
+++ course-04/project/c4-final-project-starter-code/client/src/config.ts
@@ -1,10 +1,9 @@
-// TODO: Once your application is deployed, copy an API id here so that the frontend could interact with it
-const apiId = '...'
-export const apiEndpoint = `https://${apiId}.execute-api.us-east-1.amazonaws.com/dev`
+const apiId = 'cl7en0ai4k'
+const region = 'eu-west-2'
+export const apiEndpoint = `https://${apiId}.execute-api.${region}.amazonaws.com/dev`
 
 export const authConfig = {
-  // TODO: Create an Auth0 application and copy values from it into this map
-  domain: '...',            // Auth0 domain
-  clientId: '...',          // Auth0 client id
+  domain: 'udacity-cloud-dev.eu.auth0.com',
+  clientId: 'U3kOgDdqPLrjGh232OkzVsYf3MLaeaRO',
   callbackUrl: 'http://localhost:3000/callback'
 }
